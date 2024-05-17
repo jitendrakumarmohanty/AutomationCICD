88ccf7255be35976e2c80e27a08c0de2b27c904e
jitendrakumarmohanty <161330644+jitendrakumarmohanty@users.noreply.github.com>
2024-05-17 22:27:05 +0530

    Update AddAddressJackson.java

--- javaTestproject/src/javatestproject/AddAddressJackson.java
+++ javaTestproject/src/javatestproject/AddAddressJackson.java
@@ -7,7 +7,7 @@ import org.testng.annotations.Test;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-//address updated   ;
+//address updated   ; jj
 public class AddAddressJackson {
 	
 	@Test
